fca42cf5487370c70c31d614426657196c146be5
neres <neresdev@gmail.com>
2024-11-18 09:07:40 -0300

    wip: classrooms table

--- src/components/ClassRooms/index.jsx
+++ src/components/ClassRooms/index.jsx
@@ -18,40 +18,38 @@ export default function ClassRooms(){
                 <input type='text' placeholder='Busque uma sala' className='search-classrooms-input' />
                 <button className='button-search-classrooms'> <SearchIcon  className='icon-search-classrooms'/> <p className='label-search-classrooms'>Buscar</p> </button>
             </div>
-            <div className='classrooms-container'>
-                <div className='classrooms-header'>
-                    <p>Turma</p>
-                    <p>Professor(a)</p>
-                    <p>Turno</p>
-                    <p>Número</p>
-                </div>
-
-                <div className='classrooms-body'>
-                    <p>5°A</p>
-                    <p>Mônica Torres</p>
-                    <p>Tarde</p>
-                    <p>1</p>
-                </div>
-                <div className='classrooms-body'>
-                    <p>4°B</p>
-                    <p>Paulo Garzón</p>
-                    <p>Manhã</p>
-                    <p>13</p>
-                </div>
-                <div className='classrooms-body'>
-                    <p>3°A</p>
-                    <p>Luís Gustavo</p>
-                    <p>Noite</p>
-                    <p>17</p>
-                </div>
-                <div className='classrooms-body'>
-                    <p>6°C</p>
-                    <p>Maria Silva</p>
-                    <p>Tarde</p>
-                    <p>19</p>
-                </div>
-                
-            </div>
+            <table className='table-classsrooms' cellSpacing={"0"}>
+                <tr>
+                    <th className='table-classrooms-first-th'>Turma</th>
+                    <th>Professor(a)</th>
+                    <th>Turno</th>
+                    <th className='table-classrooms-last-th'>Número</th>
+                </tr>
+                <tr>
+                    <td>5°A</td>
+                    <td>Mônica Torres</td>
+                    <td>Tarde</td>
+                    <td>1</td>
+                </tr>
+                <tr>
+                    <td>4°B</td>
+                    <td>Paulo Garzón</td>
+                    <td>Manhã</td>
+                    <td>13</td>
+                </tr>
+                <tr>
+                    <td>3°A</td>
+                    <td>Luís Gustavo</td>
+                    <td>Noite</td>
+                    <td>17</td>
+                </tr>
+                <tr>
+                    <td>6°C</td>
+                    <td>Maria Silva</td>
+                    <td>Tarde</td>
+                    <td>19</td>
+                </tr>
+            </table>
         </div>
        </div> 
     )
