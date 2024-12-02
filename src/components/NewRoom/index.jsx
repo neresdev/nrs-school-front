fe0b4b602a49bac71f859769087325dd5b0ca7da
neres <neresdev@gmail.com>
2024-12-02 20:18:58 -0300

    wip: new classrooms page

--- src/components/NewRoom/index.jsx
+++ src/components/NewRoom/index.jsx
@@ -5,6 +5,18 @@ export default function NewRoom(){
         <div className="newroom-container">
             <div className='newroom-body'>
                 <h1>Cadastrar Sala</h1>
+                <div className='newroom-line-container'>
+                    <div className='newrooom-line'>
+                        <label className='newroom-label'> <p> Turno </p> <input /> </label>    
+                        <label className='newroom-label'> <p>Professor(a)</p> <input /> </label>                    
+                    </div>
+                    <div className='newrooom-line'>
+                        <label className='newroom-label'> <p> Turno </p> <input /> </label>    
+                        <label className='newroom-label'> <p>Professor(a)</p> <input /> </label>                    
+                    </div>
+                </div>
+                
+                    
             </div>
             
         </div>
