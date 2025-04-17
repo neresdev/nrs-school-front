d6daab85d351061b61a6a9f6ad9ef9b08e9b660d
neresdev <neresdev@gmail.com>
2025-04-17 17:23:33 -0300

    calling toast and validating session token expiration

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,24 @@ import Login from "./components/Login"
 import useToken from "./hooks/useToken";
 import NewRoom from "./components/NewRoom";
 import NewStudent from "./components/NewStudent";
+import {toast, ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
+const SESSION_EXPIRED_MESSAGE = "Sua sessão expirou. Faça login novamente.";
 function App() {
   const {token, setToken} = useToken();
   
-  if(!token){
-    return <Login setToken={setToken} />
+  if(isJwtInvalid(token)){
+
+    toast.error(SESSION_EXPIRED_MESSAGE, {
+      position: "top-right",
+      autoClose: 5000,
+    });
+    
+    return  <>
+              <ToastContainer />
+              <Login setToken={setToken} />
+            </>
   }
 
   return (
@@ -25,3 +37,28 @@ function App() {
 }
 
 export default App
+
+function isJwtInvalid(token){
+  if (!token) return true;
+
+  try {
+    const payloadBase64 = token.split('.')[1];
+    const decodedPayload = JSON.parse(atob(payloadBase64));
+    
+    const currentTime = Math.floor(Date.now() / 1000); // segundos
+    
+    if (decodedPayload.exp) {
+        return decodedPayload.exp < currentTime;
+    }
+
+    if (decodedPayload.iat) {
+      const expirationTime = decodedPayload.iat + 3600; // 1 hora
+      return expirationTime < currentTime;
+    }
+
+    return true;
+  } catch (e) {
+    console.error('Erro ao verificar expiração do token:', e);
+    return true;
+  }
+}
