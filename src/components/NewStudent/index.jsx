e486af2861e59b685f555d67ee8e88a93befd2b6
neresdev <neresdev@gmail.com>
2025-04-28 00:27:41 -0300

    toast for errors and success when creating student

--- src/components/NewStudent/index.jsx
+++ src/components/NewStudent/index.jsx
@@ -2,32 +2,36 @@ import { useState } from 'react'
 import './index.css'
 import api from '../../services/api';
 import useToken from '../../hooks/useToken';
+import { toast, ToastContainer } from 'react-toastify';
+import { messageFromErrorCode } from '../../utils/apiUtils';
 
 const generateRegistration = () => {
-    const caracteres = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    let resultado = '';
+    const letters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    let result = '';
     for (let i = 0; i < 7; i++) {
-      const indice = Math.floor(Math.random() * caracteres.length);
-      resultado += caracteres[indice];
+      const index = Math.floor(Math.random() * letters.length);
+      result += letters[index];
     }
-    return resultado;
+    return result;
   }
 
 async function createStudent(data, token){
-        
-    api.post('/create/student', data,  {
+    let response = await api.post('/student/create', data,  {
         headers: {
             'Authorization': `Bearer ${token}`
         },
-    })
+    }).then(data => data).catch(data => data.response);
+    return response;
 }
 
 export default function NewStudent(){
     const [studentName, setStudentName] = useState();
     const [studentEmail, setStudentEmail] = useState();
     const [classroomName, setClassroomName] = useState();
+    const [isCreateStudentError, setIsCreateStudentError] = useState(null);
     const {token} = useToken();
 
+    
     const handleCreateStudent = async e => {
         e.preventDefault();
         const registration = generateRegistration();
@@ -37,31 +41,47 @@ export default function NewStudent(){
             classroomName,
             registration
         }
-        await createStudent(studentData, token)
+        const response = await createStudent(studentData, token);
+        if(response.status >= 400 && response.status <= 499) {
+            console.log(response.data)
+            
+            toast.error(messageFromErrorCode(response.data.errorCode), {
+                position: "top-right",
+                autoClose: 5000,
+            });
+            setIsCreateStudentError(true);
+            return;
+        }
+        toast.success('Estudante cadastrado com sucesso!', {
+            position: "top-right",
+            autoClose: 5000,
+        });        
     }
-
+    
     return (
-        <div className="newstudent-container">
-            <div className='newstudent-body'>
-                <h1>Cadastrar Estudante</h1>
-                <div className='newstudent-line-container'>
-                    <div className='newroom-line blue'>
-                        <label className='newstudent-label-name'> <p>Nome</p> <input onChange={e => setStudentName(e.target.value)} /> </label>    
+        <form onSubmit={handleCreateStudent}>
+            { isCreateStudentError != null && <ToastContainer />}
+            
+            <div className="newstudent-container">
+                <div className='newstudent-body'>
+                    <h1>Cadastrar Estudante</h1>
+                    <div className='newstudent-line-container'>
+                        <div className='newroom-line blue'>
+                            <label className='newstudent-label-name'> <p>Nome</p> <input onChange={e => setStudentName(e.target.value)} /> </label>    
+                        </div>
+                        <div className='newroom-line red'>
+                            <label className='newstudent-label'> <p>Email(a)</p> <input type="email" onChange={e => setStudentEmail(e.target.value)}/> </label>                    
+                            <label className='newstudent-label'> <p> Classe </p> <input onChange={e => setClassroomName(e.target.value)} /> </label>    
+                        </div>
+                        <div className='register-student-container'>
+                            <button className='register-student-button'>Cadastrar Sala</button>
+                        </div>
                     </div>
-                    <div className='newroom-line red'>
-                        <label className='newstudent-label'> <p>Email(a)</p> <input onChange={e => setStudentEmail(e.target.value)}/> </label>                    
-                        <label className='newstudent-label'> <p> Classe </p> <input onChange={e => setClassroomName(e.target.value)} /> </label>    
-                    </div>
-                    <div className='register-student-container'>
-                        <button className='register-student-button' onClick={handleCreateStudent}>Cadastrar Sala</button>
-                    </div>
+                    
+                        
                 </div>
                 
-                    
             </div>
-            
-        </div>
-
-        
+        </form>
     )
 }
