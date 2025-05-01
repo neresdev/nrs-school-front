3211b3b4329418fa2152730582b19d0b5e594480
neresdev <neresdev@gmail.com>
2025-04-30 22:37:48 -0300

    labels class name and new property

--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,16 +1,17 @@
 import './index.css'
 import PropTypes from 'prop-types';
 
-export default function Dashboard({studentsQuantityLabelText, studentsQuantity}){
+export default function Dashboard({studentsQuantityLabelText, studentsQuantity, dashboardClassname}){
     return (
-        <div className='dashboard'>
-            <h2 className='students-quantity-label'>{studentsQuantityLabelText}</h2>
-            <h1 className='students-quantity'>{studentsQuantity}</h1>
+        <div className={dashboardClassname != null ? dashboardClassname : 'dashboard'}>
+            <h2 className='quantity-label'>{studentsQuantityLabelText}</h2>
+            <h1 className='quantity'>{studentsQuantity}</h1>
         </div>  
     )
 }
 
 Dashboard.propTypes = {
     studentsQuantityLabelText: PropTypes.string.isRequired,
-    studentsQuantity: PropTypes.string.isRequired
+    studentsQuantity: PropTypes.string.isRequired,
+    dashboardClassname: PropTypes.string.isRequired
   }
