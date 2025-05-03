49529246b8d840ebc88ea8f0548812d6b8eed4b6
neresdev <neresdev@gmail.com>
2025-05-02 22:15:08 -0300

    take the students from that classroom

--- src/components/Students/index.jsx
+++ src/components/Students/index.jsx
@@ -1,8 +1,47 @@
 import Dashboard from "../Dashboard";
 import SearchIcon from '@mui/icons-material/Search';
 import './index.css';
+import { useLocation } from "react-router-dom";
+import useToken from "../../hooks/useToken";
+import api from "../../services/api";
+import { useEffect, useState } from "react";
 
 export default function Students() {
+    const location = useLocation();
+    const queryParams = new URLSearchParams(location.search);
+    const [students, setStudents] = useState([]);
+    const [classroomData, setClassroomData] = useState();
+    const {token} = useToken();
+    const classroomId = queryParams.get('id')
+
+    useEffect(() => {
+        api.get(`/students/${classroomId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+                
+            }
+            })
+            .then((response) => {
+                setStudents(response.data);
+        })
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+
+        api.get(`/classrooms/${classroomId}`, {
+            headers: {
+                'Authorization': `Bearer ${token}`
+                
+            }
+            })
+            .then((response) => {
+                
+                setClassroomData(response.data);
+        })
+            .catch((err) => {
+                console.error("ops! ocorreu um erro" + err);
+            });
+        }, [token, classroomId]);
 
     return (
         <div className="students-container">
@@ -11,8 +50,8 @@ export default function Students() {
                 <button className='new-room-button' >Novo aluno</button>
             </div>
             <div className='students-dashboards-container'>
-                <Dashboard studentsQuantityLabelText="Sala" studentsQuantity="4°G" dashboardClassname="students-dashboard"/>
-                <Dashboard studentsQuantityLabelText="Quantidade de alunos" studentsQuantity="30" dashboardClassname="students-dashboard"/>
+                <Dashboard studentsQuantityLabelText="Sala" studentsQuantity={classroomData?.classroomName} dashboardClassname="students-dashboard"/>
+                <Dashboard studentsQuantityLabelText="Quantidade de alunos" studentsQuantity={students.length} dashboardClassname="students-dashboard"/>
                 <Dashboard studentsQuantityLabelText="Alunos em recuperação" studentsQuantity="6" dashboardClassname="students-dashboard"/>
             </div>
             <div className='search-students-container'>
@@ -24,15 +63,18 @@ export default function Students() {
                     <tr>
                         <th>Nome</th>
                         <th>Matrícula</th>
-                        <th>Turno</th>
+                        <th>Email</th>
                     </tr>
                 </thead>
                 <tbody>
-                        <tr>
-                            <td>a</td>
-                            <td>a</td>
-                            <td>a</td>
+                    {students.map(student => (
+                        <tr key={student.registration}>
+                            <td>{student.studentName}</td>
+                            <td>{student.registration}</td>
+                            <td>{student.studentEmail}</td>
                         </tr>
+                    ))}
+                        
                     
                 </tbody>
             </table>
